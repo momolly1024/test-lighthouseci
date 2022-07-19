5513971e51f817be75de3f69327ebb3686991cf3
momolly1024 <momolly1024@gmail.com>
2022-07-19 11:54:15 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css'
 const Test = () => {
     return <div>Hello world~</div>
 }
+const Test2 = () => {
+    return <div>tqtqtqtq</div>
+}
 function App() {
     return (
         <div className='App'>
             <Test />
+            <Test2 />
         </div>
     )
 }
